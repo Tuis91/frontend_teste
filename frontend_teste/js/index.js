b8f60495b6ae06a773db221af4efd5d8f0071762
Tuis91 <104057865+Tuis91@users.noreply.github.com>
2022-06-17 12:09:56 -0400

    Add files via upload

--- frontend_teste/js/index.js
+++ frontend_teste/js/index.js
@@ -14,14 +14,17 @@ buyStickerForm.addEventListener('submit', handleSubmit);
 function handleSubmit(e) {
     e.preventDefault();
     errorbox.innerHTML = '';
+    errorbox.classList.add('invisible');
     if(validateReactCheckbox.checked == false && validateVueCheckbox.checked == false && validateAngularCheckbox.checked == false) {
         error = 'Você precisa escolher quais stickers deseja comprar! :)'
         errorbox.innerHTML = error;
+        errorbox.classList.remove('invisible');
         return
     }
     if(validateCounterNow.value == 0){
         error = 'Ops! Faltou escolher a quantidade de stickers.'
         errorbox.innerHTML = error;
+        errorbox.classList.remove('invisible');
         return
     }
     buyStickerForm.submit();
